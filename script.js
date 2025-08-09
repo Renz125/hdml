1cc73ada74e14152f0dd9b445c417c203ecf8229
Renz125 <renzortiz009@gmail.com>
2025-08-09 20:31:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function myFunction() {
-      var a = 4;
-      document.getElementById("demo").innerHTML = a*a; }
+function validation(e) {
+    e.preventDefault();
+    
+    const email = document.getElementById('email'). value;
+    const pass = document.getElementById('password'). value;
+    const age = document.getElementById('age'). value;
+    const msgBox = document.getElementById('message');
+
+    let message = '' ;
+
+    if (email === '') {
+        message = 'Enter an email.';
+        msgBox.style.color = 'red';
+    } 
+    else if (pass === '') {
+        message = 'Enter an password.';
+        msgBox.style.color = 'red';
+    }
+    else if (age === '') {
+        message = 'Enter your age.';
+        msgBox.style.color = 'red';
+    }
+    else{
+        message = 'login succesful!';
+        msgBox.style.color = 'green';
+    }
+
+    msgBox.innerText = message;
+}
