47954611269cf3ed877ec41da9912337c521fa87
Renz125 <renzortiz009@gmail.com>
2025-09-21 20:22:52 +0800

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,5 @@
+print("Hello world, welcome to codingal") #print a statement
+print(4) #print a number
+print("Hello",4) #print two arguments a string and a number
+print("you are welcome in codingal\n") # \n is a space
+print("I am very honored to be here")
