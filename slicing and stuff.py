a0946f6df12ebef6175061e0c911b2af86aa5225
Renz125 <renzortiz009@gmail.com>
2025-09-27 20:19:16 +0800

    Add files via upload

---
+++ slicing and stuff.py
@@ -0,0 +1,16 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+example = "Haa"*9
+
+print("First_Name :", first_name)
+print("Second_Name :", last_name)
+print("Full_name :", full_name)
+print("String multipied 5 times this result :", example)
+
+word = 'Coding'
+print("Length of the string:", len(word))
+print("First letter of the word:", word[0])
+print("Last letter of the word:", word[5])
+print("String sliced:", word[0:3])
+
---
+++ types of data.py
@@ -0,0 +1,21 @@
+name = "melody"
+age = 18
+is_student = True
+weight = 39.5
+
+print("Name :", name)
+print("Data type of Name is", type(name))
+print("Age :", age)
+print("Data type of Name is", type(age))
+print("is student", is_student)
+print("Data type of is_student", type(is_student))
+print("Weight :", type(weight))
+print("Data type of Name is", type(weight))
+
+print("\in After type casting.......")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
