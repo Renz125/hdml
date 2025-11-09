baef8019ef6ddc2e9f51e60dfa76032ba17466ed
Renz125 <renzortiz009@gmail.com>
2025-11-09 20:18:14 +0800

    Add files via upload

---
+++ merging.py
@@ -0,0 +1,12 @@
+with open('sample_doc.txt') as fp:
+    data1 = fp.read()
+
+with open('Codingal.txt') as fp:
+    data2 = fp.read()
+
+data1 += "\n"
+data1 +=  data2
+print("Merging two files.....")
+with open('Merged_File.txt', 'w')as fp:
+    fp.write(data1)
+
