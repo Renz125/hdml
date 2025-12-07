bcb784baec78c6bb977ec580f7c0405b3da05b2e
Renz125 <renzortiz009@gmail.com>
2025-12-07 20:19:55 +0800

    Add files via upload

---
+++ MultiplybyN.py
@@ -0,0 +1,15 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a second number: "))
+
+def myfunction(a,b):
+    mul = a*b
+    print(mul)
+
+def myFunction(n):
+    for i in range(n):
+        mul = a*b
+        print(mul)
+
+n = int(input("Enter iterations: "))
+myFunction(n)
+myfunction(a,b)
