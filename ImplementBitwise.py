55b335ebaa4c6c826c7154a2762b715f9a3ea103
Renz125 <renzortiz009@gmail.com>
2025-12-14 20:08:13 +0800

    Add files via upload

---
+++ ImplementBitwise.py
@@ -0,0 +1,32 @@
+print("Given numbers/ num1 = 10 , num2 = 4")
+
+num1 = 10
+num2 = 4
+
+#Used AND
+print("num1 & num2 =", num1 & num2)
+
+#Used OR
+print("num1 | num2 = ", num1 | num2)
+
+#No operator
+print("|n-num1 =", ~num1 )
+
+#XOR operator
+print("\nnum1 ^ num2 = ", num1 ^ num2)
+
+#Used Right Shift Operator
+num1 = 10
+num2 = 4
+
+print("\nnum1 >> 1 =", num1 >> 1)
+
+print("\nnum2 >> 1 =", num2 >> 1)
+
+#Used left Shift operator
+num1 = 10
+num2 = 4
+
+print("\nnum1 << 1 =", num1 << 1)
+
+print("nnum2 << 1 =", num2 << 1)
