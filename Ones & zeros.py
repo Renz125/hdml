40efffe51e3c6bfebdc02eda20b1f5cbd6e2ea75
Renz125 <renzortiz009@gmail.com>
2025-12-20 19:56:23 +0800

    Add files via upload

---
+++ Ones & zeros.py
@@ -0,0 +1,18 @@
+def numbersOfBits(n):
+    ones = 0
+    zeros = 0
+
+    #while our number is greater than zero check last bit and right shift
+    while (n):
+        
+        # use AND operator to check if last bit is 1 or 0
+        if (n&1 == 1):
+            ones += 1
+        else:
+            zeros += 1
+        #Right shift the number remove the last bit that we just checked above
+        n >>= 1
+    print("\n\nOnes = ", ones, "\nZeros ", zeros)
+
+number = int(input("Enter a number"))
+numbersOfBits(number)
